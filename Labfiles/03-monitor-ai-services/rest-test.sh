5d7be833e0903a75f38630cc0a059ca914ef28f6
saikrmet <saikrmet@gmail.com>
2024-08-23 11:13:06 -0400

    Script edits

--- Labfiles/03-monitor-ai-services/rest-test.sh
+++ Labfiles/03-monitor-ai-services/rest-test.sh
@@ -1,1 +1,3 @@
 curl -X POST "<your-endpoint>/text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <your-key>" --data-ascii "{'documents':           [{'id':1,'text':'hello'}]}"
+
+#Tests total number of requests metric
--- Labfiles/04-use-a-container/rest-test.sh
+++ Labfiles/04-use-a-container/rest-test.sh
@@ -1,1 +1,3 @@
 curl -X POST "http://<your-FQDN>:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
+
+# Tests containerized instance of AI Language service
